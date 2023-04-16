8b2016d06f25f34e8f93bde1c8b0cc81aeb8081b
Elyas <Elyas.shamal2@gmail.com>
2023-04-15 20:24:27 -0400

    JavaScript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+
+// function addingEventListener() {
+//     const btn = document.getElementById('button');
+
+//     function clickAlert(){
+//         alert('i was clicked !')
+//     }
+
+//     btn.addEventListener('click', clickAlert);
+// }
+
+// addingEventListener()
+
 function addingEventListener() {
+    const btn = document.getElementById('button');
+    btn.addEventListener('click', function(){
+        alert('i was clicked !')
+    })
 }
+
+addingEventListener()
